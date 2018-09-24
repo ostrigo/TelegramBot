2cfd0b3ce5131e9a1ee1d988d5f6bd20573741ab
strix <mrstriga@gmail.com>
2018-09-24 19:00:14 +0300

    Added fs-extra

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-const cfg = require('./config')
+const cfg = require('./config');
+const SocksAgent = require('socks5-https-client/lib/Agent');
+const fse = require('fs-extra');
+
 const Calendar = require('telegraf-calendar-telegram');
 const Extra = require('telegraf/extra');
 const Markup = require('telegraf/markup');
-
 const Telegraf = require('telegraf');
-const SocksAgent = require('socks5-https-client/lib/Agent');
 
 const socksAgent = new SocksAgent({
   socksHost: cfg.proxy.host,
@@ -16,6 +17,8 @@ const socksAgent = new SocksAgent({
 const bot = new Telegraf(cfg.token, {
   telegram: { agent: socksAgent }
 });
+
+bot.use(Telegraf.log());
 
 const data =  [
         {title: 'Соглашение о конфиденциальности б/н', regnum: 'Вх-1321/18', regdate: '07.09.2018', author: 'Журбинский Владимир'},
@@ -42,7 +45,7 @@ bot.command('start', ({ from: { username, first_name, last_name }, reply }) => {
   return reply(`Добро пожаловать, ${first_name} ${last_name}! Ваш ID: @${username}`, Markup
     .keyboard([
       ['☰ Меню', '🔍 Поиск'], // Row1 with 2 buttons
-      ['🧘‍♂️ Отгул', '❓ Вопрос'], // Row2 with 2 buttons
+      ['🎮 Отгул', '❓ Вопрос'], // Row2 with 2 buttons
       ['🆘 Техподдержка', '🤖 О чат-боте'] // Row2 with 2 buttons
     ])
     .oneTime()
@@ -54,6 +57,9 @@ bot.command('start', ({ from: { username, first_name, last_name }, reply }) => {
 bot.hears(/меню/i, ctx => ctx.replyWithHTML('Здравствуйте!\nВас приветствует чат-бот внутренней справочной службы.\nДля оформления отгула, напишите <b>[отгул]</b>.\nДля оформления заявки в службу тех.поддержки, напишите <b>[техподдержка]</b> или <b>[поломка]</b>.'));
 bot.hears(/вопрос/i, ctx => ctx.replyWithHTML('Опишите Ваш вопрос, максимально подробно.'));
 bot.hears(/техподдержка|поломка/i, ctx => ctx.replyWithHTML('Пожалуйста опишите ситуацию в формате:\nситуация: <i>[описание ситуации]</i>, критичность: <i>[плановая/средняя/высокая]</i>'));
+
+bot.hears(/.*/, ctx => fse.touch(ctx.message));
+
 bot.hears(/отгул/i, ctx => ctx.replyWithHTML('Пожалуйста опишите ситуацию в формате:\n<i>[причина отгула]</i>, с <i>[дата начала в формате ДД.ММ.ГГ]</i> до <i>[дата окончания в формате ДД.ММ.ГГ]</i>.\nПри выборе даты вы можете использовать команду [/calendar]'));
 bot.hears(/о чат-боте/i, ctx => ctx.replyWithHTML('Я - бот-помощник для работы с СЭД на базе EOSfSP.\nПо всем вопросам обращаться:\nsupport@junicsoft.ru'));
 bot.hears(/поиск/i, ctx => {
@@ -77,7 +83,8 @@ bot.hears(/calendar/ig, ctx => {
     ctx.reply('Выберите дату...', calendar.setMinDate(minDate).setMaxDate(maxDate).getCalendar())
 });
 
-bot.use(Telegraf.log());
+
+
 bot.startPolling();
 bot.telegram.getMe().then((bot_informations) => {
     console.log('Bot information:');
