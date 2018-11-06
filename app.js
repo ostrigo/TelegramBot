600ea1f34dae17bb96a95b6f2a860a6f32b5389d
Oleg Strigo <mrstriga@gmail.com>
2018-11-06 21:56:32 +0300

    Pretty + log on

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const socksAgent = new SocksAgent({
     socksHost: cfg.proxy.host,
     socksPort: cfg.proxy.port,
     socksUsername: cfg.proxy.login,
-    socksPassword: cfg.proxy.psswd,
+    socksPassword: cfg.proxy.psswd
 });
 // create the bot
 const bot = new Telegraf(cfg.token, {
@@ -27,27 +27,62 @@ let chatId;
 let msgId;
 let json = {};
 
-// bot.use(Telegraf.log());
+bot.use(Telegraf.log());
 
-bot.catch((err) => {
-    console.log('Ooops: ', err)
-})
+bot.catch(err => {
+    console.log('Ooops: ', err);
+});
 
 const data = [
-    { title: 'Соглашение о конфиденциальности б/н', regnum: 'Вх-1321/18', regdate: '07.09.2018', author: 'Журбинский Владимир' },
-    { title: 'Акт сверки за 1 полугодие 2018', regnum: 'Вх-1318/18', regdate: '07.09.2018', author: 'Журбинский Владимир' },
-    { title: 'О продлении действия льготного периода №231 от 30.08.2018', regnum: 'Вх-1309/18', regdate: '06.09.2018', author: 'Стриго Олег' },
-    { title: 'Счета за август+ протокол согласования цены №4 от 01.08.2018', regnum: 'Вх-1285/18', regdate: '04.09.2018', author: 'Стриго Олег' },
-    { title: 'Сертификаты + сопроводительное письмо №8310 от 16.08.2018', regnum: 'Вх-1211/18', regdate: '29.08.2018', author: 'Юферев Андрей' },
+    {
+        title: 'Соглашение о конфиденциальности б/н',
+        regnum: 'Вх-1321/18',
+        regdate: '07.09.2018',
+        author: 'Журбинский Владимир'
+    },
+    {
+        title: 'Акт сверки за 1 полугодие 2018',
+        regnum: 'Вх-1318/18',
+        regdate: '07.09.2018',
+        author: 'Журбинский Владимир'
+    },
+    {
+        title: 'О продлении действия льготного периода №231 от 30.08.2018',
+        regnum: 'Вх-1309/18',
+        regdate: '06.09.2018',
+        author: 'Стриго Олег'
+    },
+    {
+        title: 'Счета за август+ протокол согласования цены №4 от 01.08.2018',
+        regnum: 'Вх-1285/18',
+        regdate: '04.09.2018',
+        author: 'Стриго Олег'
+    },
+    {
+        title: 'Сертификаты + сопроводительное письмо №8310 от 16.08.2018',
+        regnum: 'Вх-1211/18',
+        regdate: '29.08.2018',
+        author: 'Юферев Андрей'
+    }
 ];
 
 // instantiate the calendar
 const calendar = new Calendar(bot, {
     startWeekDay: 1,
-    weekDayNames: ["Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Вс"],
+    weekDayNames: ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс'],
     monthNames: [
-        "Янв", "Фев", "Мар", "Апр", "Май", "Июн",
-        "Июл", "Авг", "Сен", "Окт", "Ноя", "Дек"
+        'Янв',
+        'Фев',
+        'Мар',
+        'Апр',
+        'Май',
+        'Июн',
+        'Июл',
+        'Авг',
+        'Сен',
+        'Окт',
+        'Ноя',
+        'Дек'
     ]
 });
 
@@ -55,56 +90,68 @@ const calendar = new Calendar(bot, {
 calendar.setDateListener((ctx, date) => ctx.reply(date));
 
 bot.command('start', ({ from: { username, first_name, last_name }, reply }) => {
-    return reply(`Добро пожаловать, ${first_name} ${last_name}! Ваш ID: @${username}`, Markup
-        .keyboard([
+    return reply(
+        `Добро пожаловать, ${first_name} ${last_name}! Ваш ID: @${username}`,
+        Markup.keyboard([
             ['☰ Меню', '🔍 Поиск'], // Row1 with 2 buttons
             ['🎮 Отгул', '❓ Вопрос'], // Row2 with 2 buttons
             ['🆘 Техподдержка', '🤖 О чат-боте'] // Row3 with 2 buttons
         ])
-        // .oneTime()
-        .resize()
-        .extra()
-    )
-})
+            // .oneTime()
+            .resize()
+            .extra()
+    );
+});
 
-bot.hears(/меню/i, ctx => ctx.replyWithMarkdown('Здравствуйте!\nВас приветствует чат-бот внутренней справочной службы.\nДля оформления отгула, напишите *[отгул]*.\nДля оформления заявки в службу тех.поддержки, напишите *[техподдержка]* или *[поломка]*.'));
-bot.hears(/вопрос/i, ctx => ctx.replyWithMarkdown('Опишите Ваш вопрос, максимально подробно.'));
+bot.hears(/меню/i, ctx =>
+    ctx.replyWithMarkdown(
+        'Здравствуйте!\nВас приветствует чат-бот внутренней справочной службы.\nДля оформления отгула, напишите *[отгул]*.\nДля оформления заявки в службу тех.поддержки, напишите *[техподдержка]* или *[поломка]*.'
+    )
+);
+bot.hears(/вопрос/i, ctx =>
+    ctx.replyWithMarkdown('Опишите Ваш вопрос, максимально подробно.')
+);
 bot.hears(/техподдержка|поломка/i, ctx => {
-    ctx.replyWithMarkdown('*Сейчас по шагам опишите ситуацию.*')
-        .then(() => {
-            ctx.replyWithMarkdown('*ТП. Шаг 1 - описание.*');
-            bot.on('text', ctx => {
-                chatId = ctx.message.chat.id;
-                msgId = ctx.message.message_id;
-                file = filesPath + 'tbot_' + chatId + '-' + msgId + '.json';
-                json = ctx.update;
-                ctx.replyWithMarkdown('*Спасибо, текст заявки сохранен!\nТП. Шаг 2 - критичность.*', Markup.inlineKeyboard([
+    ctx.replyWithMarkdown('*Сейчас по шагам опишите ситуацию.*').then(() => {
+        ctx.replyWithMarkdown('*ТП. Шаг 1 - описание.*');
+        bot.on('text', ctx => {
+            chatId = ctx.message.chat.id;
+            msgId = ctx.message.message_id;
+            file = filesPath + 'tbot_' + chatId + '-' + msgId + '.json';
+            json = ctx.update;
+            ctx.replyWithMarkdown(
+                '*Спасибо, текст заявки сохранен!\nТП. Шаг 2 - критичность.*',
+                Markup.inlineKeyboard([
                     Markup.callbackButton('Плановая', 'crit1'),
                     Markup.callbackButton('Средняя', 'crit2'),
                     Markup.callbackButton('Высокая', 'crit3')
-                ])
-                    .extra())
-            });
+                ]).extra()
+            );
+        });
 
-            function setCrit(ctx, lvl) {
-                json['support_crit'] = lvl;
-                return ctx.replyWithMarkdown(`*Выбрана критичность: [${lvl}].\nТП. Шаг 3 - фото.*`);
-            }
-            bot.action('crit1', ctx => {
-                setCrit(ctx, 'Плановая');
-            });
-            bot.action('crit2', ctx => {
-                setCrit(ctx, 'Средняя');
-            });
-            bot.action('crit3', ctx => {
-                setCrit(ctx, 'Высокая');
-            });
-            // Handle sticker or photo update
-            bot.on(['sticker', 'photo'], (ctx) => {
-                const photoId = ctx.message.photo[ctx.message.photo.length - 1].file_id;
-                bot.telegram.getFileLink(photoId).then(result => {
-                    const filename = 'tbot_' + chatId + '-' + msgId + '.jpg';
-                    request.get({
+        function setCrit(ctx, lvl) {
+            json['support_crit'] = lvl;
+            return ctx.replyWithMarkdown(
+                `*Выбрана критичность: [${lvl}].\nТП. Шаг 3 - фото.*`
+            );
+        }
+        bot.action('crit1', ctx => {
+            setCrit(ctx, 'Плановая');
+        });
+        bot.action('crit2', ctx => {
+            setCrit(ctx, 'Средняя');
+        });
+        bot.action('crit3', ctx => {
+            setCrit(ctx, 'Высокая');
+        });
+        // Handle sticker or photo update
+        bot.on(['sticker', 'photo'], ctx => {
+            const photoId =
+                ctx.message.photo[ctx.message.photo.length - 1].file_id;
+            bot.telegram.getFileLink(photoId).then(result => {
+                const filename = 'tbot_' + chatId + '-' + msgId + '.jpg';
+                request
+                    .get({
                         url: result,
                         agentClass: SocksAgent,
                         agentOptions: {
@@ -114,50 +161,81 @@ bot.hears(/техподдержка|поломка/i, ctx => {
                             socksPassword: cfg.proxy.psswd
                         }
                     })
-                        .on('error', function (err) {
-                            console.log(err);
-                        })
-                        .pipe(fse.createWriteStream(path.join(filesPath, filename)));
-                    fse.writeFile(file, JSON.stringify(json, null, 4), 'utf-8', function (err) {
+                    .on('error', function(err) {
+                        console.log(err);
+                    })
+                    .pipe(
+                        fse.createWriteStream(path.join(filesPath, filename))
+                    );
+                fse.writeFile(
+                    file,
+                    JSON.stringify(json, null, 4),
+                    'utf-8',
+                    function(err) {
                         if (err) {
                             console.error(err);
-                            ctx.reply('Ошибка синхронизации с СЭД! Попробуйте еще раз.');
+                            ctx.reply(
+                                'Ошибка синхронизации с СЭД! Попробуйте еще раз.'
+                            );
                             return;
-                        };
-                        console.log('*************** Files has been created ***************');
-                    })
-                    ctx.replyWithMarkdown('*Спасибо, заявка отправлена в СЭД для модерации!*');
-                });
+                        }
+                        console.log(
+                            '*************** Files has been created ***************'
+                        );
+                    }
+                );
+                ctx.replyWithMarkdown(
+                    '*Спасибо, заявка отправлена в СЭД для модерации!*'
+                );
             });
-        })
+        });
+    });
 });
 
-bot.hears(/отгул/i, ctx => ctx.replyWithMarkdown('Пожалуйста опишите ситуацию в формате:\n_[причина отгула]_, с _[дата начала в формате ДД.ММ.ГГ]_ до _[дата окончания в формате ДД.ММ.ГГ]_.\nПри выборе даты вы можете использовать команду [/calendar]'));
-bot.hears(/о чат-боте/i, ctx => ctx.replyWithMarkdown('Я - бот-помощник для работы с СЭД на базе EOSfSP.\nПо всем вопросам обращаться:\nsupport@junicsoft.ru'));
+bot.hears(/отгул/i, ctx =>
+    ctx.replyWithMarkdown(
+        'Пожалуйста опишите ситуацию в формате:\n_[причина отгула]_, с _[дата начала в формате ДД.ММ.ГГ]_ до _[дата окончания в формате ДД.ММ.ГГ]_.\nПри выборе даты вы можете использовать команду [/calendar]'
+    )
+);
+bot.hears(/о чат-боте/i, ctx =>
+    ctx.replyWithMarkdown(
+        'Я - бот-помощник для работы с СЭД на базе EOSfSP.\nПо всем вопросам обращаться:\nsupport@junicsoft.ru'
+    )
+);
 bot.hears(/поиск/i, ctx => {
-    ctx.replyWithMarkdown('*Активные документы:*')
-        .then(() => {
-            data.forEach((item) => {
-                ctx.replyWithMarkdown(`[${item.regnum}](https://junicsoft.ru) от ${item.regdate}`);
-            })
+    ctx.replyWithMarkdown('*Активные документы:*').then(() => {
+        data.forEach(item => {
+            ctx.replyWithMarkdown(
+                `[${item.regnum}](https://junicsoft.ru) от ${item.regdate}`
+            );
         });
+    });
 });
 
 // retreive the calendar HTML
-bot.hears(/calendar/ig, ctx => {
-
+bot.hears(/calendar/gi, ctx => {
     const today = new Date();
     const minDate = new Date();
     minDate.setMonth(today.getMonth() - 2);
     const maxDate = new Date();
     maxDate.setMonth(today.getMonth() + 2);
 
-    ctx.reply('Выберите дату...', calendar.setMinDate(minDate).setMaxDate(maxDate).getCalendar())
+    ctx.reply(
+        'Выберите дату...',
+        calendar
+            .setMinDate(minDate)
+            .setMaxDate(maxDate)
+            .getCalendar()
+    );
 });
 
 bot.startPolling();
-bot.telegram.getMe().then((bot_informations) => {
+bot.telegram.getMe().then(bot_informations => {
     console.log('Bot information:');
     console.log(bot_informations);
-    console.log('Server has initialized bot nickname. Nick: ' + bot_informations.username + '\r\n');
+    console.log(
+        'Server has initialized bot nickname. Nick: ' +
+            bot_informations.username +
+            '\r\n'
+    );
 });
